8955ce8b11ebcbeb9f00fd7087ee9425425bbc8b
YangDDgood <143944770+YangDDgood@users.noreply.github.com>
2024-06-16 21:50:01 +0800

    Delete ECWT_FOR_DEFLETOMATRY directory

--- ECWT_FOR_DEFLETOMATRY/Empirical Wavelet Transform/readme.txt
+++
@@ -1,1 +0,0 @@
-The full version of EWT refer to https://ww2.mathworks.cn/matlabcentral/fileexchange/42141-empirical-wavelet-transforms by Jerome Gilles.
--- ECWT_FOR_DEFLETOMATRY/utils/findTwoMaxPositions.m
+++
@@ -1,6 +0,0 @@
-function [max_positions] = findTwoMaxPositions(arr)
-    [~, max_index1] = max(arr);
-    arr(max_index1) = -inf; 
-    [~, max_index2] = max(arr);
-    max_positions = [max_index1, max_index2];
-end
